22c9a1d34f97bf76d31de280baadd1ceec436428
hendisantika <hendisantika@yahoo.co.id>
2024-12-08 07:51:49 +0700

    Modify main app🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/flink/SpringBootFlinkApplication.java
+++ src/main/java/id/my/hendisantika/flink/SpringBootFlinkApplication.java
@@ -2,12 +2,22 @@ package id.my.hendisantika.flink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFlinkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootFlinkApplication.class, args);
+//        SpringApplication.run(SpringBootFlinkApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootFlinkApplication.class, args);
+
+        // 获取 FlinkJobService Bean，并运行 Flink 作业
+        FlinkJobService flinkJobService = context.getBean(FlinkJobService.class);
+        try {
+            flinkJobService.runFlinkJob();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
