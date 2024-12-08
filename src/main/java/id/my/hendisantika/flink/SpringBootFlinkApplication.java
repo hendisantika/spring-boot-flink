3bec74666dd1ef4eadd92fa159f3cbea0a3fd3b7
hendisantika <hendisantika@yahoo.co.id>
2024-12-08 07:49:10 +0700

    Create Spring Boot🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/flink/SpringBootFlinkApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/flink/SpringBootFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
